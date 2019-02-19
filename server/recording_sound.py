6185caefdf63855fe626a0729ebf7a4bc9058b80
JulianMich00 <julianmichelsen00@gmail.com>
2019-02-19 12:30:07 +0000

    drone_init.txt sent

--- server/recording_sound.py
+++ server/recording_sound.py
@@ -4,27 +4,31 @@ import soundfile as sf
 import git
 import os
 import speech_recognition as sr
+import time 
+
+#sample rate same as necessary by the inbuilt mic, change as necessary
+fs = 48000
+sd.default.samplerate = fs
+
+#duration of sound sample, change as necessary
+duration = 5
 
 #file that executes drone movement
 f_drone = 'drone_init.txt'
 f_sound1 = 'sound1.wav'
 f_sound2 = 'sound2.wav'
 
+#find repo as well as add commit message
+repo_dir = '~/MediDrone'
+commit_message = 'drone_init.txt sent'
+
 #reset a file in git
 def reset(f_name):
     if os.path.isfile(f_name):
         os.remove(f_name)
 
-reset(f_drone)
-reset(f_sound1)
-reset(f_sound2)
-
-#find repo as well as add commit message
-repo_dir = '~/MediDrone'
-commit_message = 'drone_init.txt sent'
-
 #function to push all
-def git_push():
+def GitPush():
     try:
         repo = git.Repo(repo_dir)
         repo.git.add('--all')
@@ -36,29 +40,34 @@ def git_push():
     finally:
         print('done push!')
 
-#sample rate same as necessary by the inbuilt mic, change as necessary
-fs = 48000
-sd.default.samplerate = fs
+#conversion between sound file and analysed text
+def SpeechAnalysis(input_sound):
+    r = sr.Recognizer()
+    sound = sr.AudioFile(input_sound)
+    with sound as source:
+        audio = r.record(source)
+    try:
+        message = r.recognize_google(audio)
+        print(message)
+        if ('help' in message) and ('medic' in message):
+            GitPush()
+            print('sending out drone')
+    except:
+        print('no voice detected this block')
+        
+        
+reset(f_drone)
+reset(f_sound1)
+reset(f_sound2)
 
-#duration of sound sample, change as necessary
-duration = 5
-
-#record sound
-print('recording!')
-curr_data = sd.rec(int(duration * fs), samplerate=fs, channels=2)
-sd.wait(10)
-
-#saving sound file
-sf.write(f_sound1, curr_data, fs)
-
-
-#checking for keywords
-r = sr.Recognizer()
-s1 = sr.AudioFile(f_sound1)
-with s1 as source:
-    audio = r.record(source)
-print('got here')
-print(r.recognize_google(audio))
-
-
-git_push()
+while True:
+    print('recording!')
+    curr_data = sd.rec(int(duration * fs), samplerate=fs, channels=2)
+    time.sleep(duration)
+    print('stopped')
+    
+    #saving sound file
+    sf.write(f_sound1, curr_data, fs)
+    sd.wait(10)
+    SpeechAnalysis(f_sound1)
+    
