92344275c6f3619647fa06c32e6d13b88f6cd5d3
JulianMich00 <julianmichelsen00@gmail.com>
2019-02-19 14:07:47 +0000

    drone_init.txt sent

--- server/recording_sound.py
+++ server/recording_sound.py
@@ -53,7 +53,7 @@ def SpeechAnalysis(input_sound):
         print(message)
         if ('help' in message) and ('medic' in message):
             file = open(f_drone, "w")
-            file.write("400 300")
+            file.write("0 100")
             file.close()
             GitPush(f_drone)
             print('sending drone file')
